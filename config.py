c2128ddfcc07476a0d9979f1739dd226c753487f
Shrejal Adhikari <s.adhikari.598@westcliff.edu>
2025-01-07 15:17:49 +0545

    Updated config.py to use PostgreSQL connection string

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 class Config:
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///your-database.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://shrejal:D8e7CGRzAKOgJofpUNtvl58ygykpbhsi@dpg-ctude03v2p9s738qibjg-a.oregon-postgres.render.com/database_3ggf'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
