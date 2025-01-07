19922e166cc524da4d8ac12df73e6ead3c6d63b0
Shrejal Adhikari <s.adhikari.598@westcliff.edu>
2025-01-07 14:11:52 +0545

    Updated init_db.py for database configuration

--- init_db.py
+++ init_db.py
@@ -1,5 +1,9 @@
-# init_db.py
 from app.db import init_db
+from dotenv import load_dotenv
+import os
+
+# Load the .env file from the root directory
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 if __name__ == '__main__':
     init_db()
